8c9d7932a15e7a7b5054c7f073b43ba6293a3d27
rimonsh <rimonsh@mta.ac.il>
2023-05-17 17:53:13 +0300

    adding poll view to event page
    Signed-off-by: rimonsh <rimonsh@mta.ac.il>

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,9 @@
 # from django.shortcuts import render
+from django.shortcuts import render
+from .models import Poll
 
-# Create your views here.
+
+def view_poll(request):
+    poll_id = request.GET["id"]
+    poll = Poll.manager.get(id=poll_id)
+    return render(request, 'event/event_info.html', {'poll': poll})
